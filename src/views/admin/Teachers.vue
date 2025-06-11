e1e50be6742ccca935eea11b59eb590142ea2c20
Dream <2965106733@qq.com>
2025-06-11 21:33:20 +0800

    2025.6.11

--- src/views/admin/Teachers.vue
+++ src/views/admin/Teachers.vue
@@ -1,11 +1,253 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { ElMessageBox, ElMessage } from 'element-plus';
+
+const searchUser = ref({name: '', gender: ''}
+
+const user = ref({
+  id: '',              // 用户唯一ID
+  username: '',          // 用户名
+  password: '',          // 加密后的密码
+  name: '',              // 姓名
+  gender: '',            // 性别，可为 '男' / '女' / ''
+  identifier: '',        // 学号/教师号/管理员号
+  subject: '',           // 教师教学科目（仅教师）
+  createTime: '',        // 注册时间（字符串）
+  updateTime: ''         // 最后修改时间（字符串）
+})
+
 
 </script>
 
 <template>
-老师管理
+  <h1>员工管理</h1>
+  <div class="container">
+    <el-form :inline="true" :model="searchUser" class="demo-form-inline">
+      <el-form-item label="姓名">
+        <el-input v-model="searchUser.name" placeholder="请输入员工姓名" clearable />
+      </el-form-item>
+
+      <el-form-item label="性别">
+        <el-select v-model="searchUser.gender" placeholder="请选择性别" clearable>
+          <el-option v-for="g in genders" :key="g.value" :label="g.name" :value="g.value" />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="">查询</el-button>
+        <el-button type="primary" @click="">清空</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <div class=container>
+    <el-button type="primary" @click="">新增员工</el-button>
+    <el-button type="danger" @click="">批量删除</el-button>
+  </div>
+
+  <div class="container">
+    <el-table :data="user" border style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column prop="name" label="姓名" width="120" align="center" />
+      <el-table-column label="性别" width="120" align="center">
+        <template #default="scope">
+          {{ scope.row.gender == '1' ? '男' : '女' }}
+        </template>
+      </el-table-column>
+      {/* 头像 */}
+      {/* <el-table-column prop="image" label="头像" width="120" align="center">
+        <template #default="scope">
+          <img :src="scope.row.image" alt="头像" style="height: 40px" align="center" />
+        </template>
+      </el-table-column> */}
+      <el-table-column prop="identifier" label="学号" width="120" align="center" />
+      <el-table-column prop="subject" label="教学科目" width="120" align="center" />
+      <el-table-column prop="udpateTime" label="最后操作时间" width="200" align="center" />
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <el-button type="primary" @click="edit(scope.row.id)">编辑</el-button>
+          <el-button type="danger" @click="deleteById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+  <div class="container">
+    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize"
+      :page-sizes="[5, 10, 20, 50, 75, 100]" :background="background" layout="total, sizes, prev, pager, next, jumper"
+      :total="total" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+  </div>
+
+  <el-dialog v-model="dialogVisible" :title="dialogTitle">
+    <el-form :model="employee" label-width="80px" :rules="rules" ref="empFormRef">
+      <!-- 基本信息 -->
+      <!-- 第一行 -->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="employee.username" placeholder="请输入员工用户名，2-20个字"></el-input>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="12">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="employee.name" placeholder="请输入员工姓名，2-10个字"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <!-- 第二行 -->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="性别" prop="gender">
+            <el-select v-model="employee.gender" placeholder="请选择性别" style="width: 100%;">
+              <el-option v-for="g in genders" :key="g.value" :label="g.name" :value="g.value" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="12">
+          <el-form-item label="手机号" prop="phone">
+            <el-input v-model="employee.phone" placeholder="请输入员工手机号"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <!-- 第三行 -->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="职位">
+            <el-select v-model="employee.job" placeholder="请选择职位" style="width: 100%;">
+              <el-option v-for="j in jobs" :key="j.value" :label="j.name" :value="j.value" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="薪资">
+            <el-input v-model="employee.salary" placeholder="请输入员工薪资"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <!-- 第四行 -->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="所属部门">
+            <el-select v-model="employee.deptId" placeholder="请选择部门" style="width: 100%;">
+              <el-option v-for="d in depts" :key="d.id" :label="d.name" :value="d.id"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="入职日期">
+            <el-date-picker v-model="employee.entryDate" type="date" style="width: 100%;" placeholder="选择日期"
+              format="YYYY-MM-DD" value-format="YYYY-MM-DD"></el-date-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <!-- 第五行 -->
+      <el-row :gutter="20">
+        <el-col :span="24">
+          <el-form-item label="头像">
+            <el-upload class="avatar-uploader" action="/api/upload" :show-file-list="false" :headers="{'token':token}"
+              :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+              <img v-if="employee.image" :src="employee.image" class="avatar" />
+              <el-icon v-else class="avatar-uploader-icon">
+                <Plus />
+              </el-icon>
+            </el-upload>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+
+      <!-- 工作经历 -->
+      <!-- 第六行 -->
+      <el-row :gutter="10">
+        <el-col :span="24">
+          <el-form-item label="工作经历">
+            <el-button type="success" size="small" @click="addExprItem">+ 添加工作经历</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <!-- 第七行 ...  工作经历 -->
+      <el-row :gutter="3" v-for="(expr, index) in employee.exprList" :key="expr.id">
+        <el-col :span="10">
+          <el-form-item size="small" label="时间" label-width="80px">
+            <el-date-picker type="daterange" v-model="expr.exprDate" range-separator="至" start-placeholder="开始日期"
+              end-placeholder="结束日期" format="YYYY-MM-DD" value-format="YYYY-MM-DD"></el-date-picker>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="6">
+          <el-form-item size="small" label="公司" label-width="60px">
+            <el-input placeholder="请输入公司名称" v-model="expr.company"></el-input>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="6">
+          <el-form-item size="small" label="职位" label-width="60px">
+            <el-input placeholder="请输入职位" v-model="expr.job"></el-input>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="2">
+          <el-form-item size="small" label-width="0px">
+            <el-button type="danger" @click="deleteExprItem(index)">- 删除</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+
+    <!-- 底部按钮 -->
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="save">保存</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
-<style scoped>
 
+<style scoped>
+.container {
+  padding: 10px;
+}
+
+.avatar {
+  height: 40px;
+}
+
+.avatar-uploader .avatar {
+  width: 78px;
+  height: 78px;
+  display: block;
+}
+
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 78px;
+  height: 78px;
+  text-align: center;
+  border-radius: 10px;
+  /* 添加灰色的虚线边框 */
+  border: 1px dashed var(--el-border-color);
+}
 </style>
